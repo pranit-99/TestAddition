5ec0680c0e8a4d0971dff98f6cb739a93fee1d54
Pranit2025 <PMhatre@clarku.edu>
2025-10-18 20:54:57 -0400

    Initial commit - Added Java addition code

---
+++ Addition/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App
+{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int a,b,c;
+
+        System.out.println("Enter the First Number:");
+        a = scanner.nextInt();
+
+        System.out.println("Enter the Second Number:");
+        b = scanner.nextInt();
+
+        scanner.close();
+
+        c = a + b;
+
+        System.out.println("Result of Addition:-"+c);
+    }
+}
